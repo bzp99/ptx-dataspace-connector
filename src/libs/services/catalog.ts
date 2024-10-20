6b072d788995011e35fd8f2dba5e2e547b7b91f5
Botond Klenk <botondklenk@gmail.com>
2024-10-20 15:55:17 +0200

    add token to catalog requests this is not the right working, it excepts, that there is only one catalog in the system

--- src/libs/services/catalog.ts
+++ src/libs/services/catalog.ts
@@ -1,5 +1,14 @@
 import axios from 'axios';
+import { generateBearerTokenFromSecret } from '../jwt';
 
 export const getCatalogData = async (endpoint: string, options?: any) => {
+    const { token } = await generateBearerTokenFromSecret();
+    if (!options) {
+        options = {};
+    }
+    if (!options.headers) {
+        options.headers = {};
+    }
+    options.headers['Authorization'] = `Bearer ${token}`;
     return axios.get(endpoint, options);
 };
